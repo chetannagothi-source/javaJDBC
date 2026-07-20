4414e8b0eca33e49032eac2b3a735779afa8ea22
chetannagothi-source <chetan.nagothi@gmail.com>
2026-07-20 11:57:29 +0530

    For n number entries

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,24 @@ import java.util.*;
 void main(){
   try{
     Scanner sc=new Scanner(System.in);
+    System.out.println("How many records:");
+    int n = sc.nextInt();
+    int rows=0;
     Class.forName("oracle.jdbc.driver.OracleDriver");
     Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","SYSTEM","Nagothi");
-    System.out.println("Enter Student Name:");
-    String sname = sc.next();
-    System.out.println("Enter Student No:");
-    int sno = sc.nextInt();
-    System.out.println("Enter Student CGPA:");
-    float cgpa = sc.nextFloat();
     PreparedStatement pstmt=con.prepareStatement("insert into student values(?,?,?)");
-    pstmt.setInt(1,sno);
-    pstmt.setString(2,sname);
-    pstmt.setFloat(3,cgpa);
-    int rows =pstmt.executeUpdate();
+    for(int i=0;i<n;i++){
+      System.out.println("Enter Student Name:");
+      String sname = sc.next();
+      System.out.println("Enter Student No:");
+      int sno = sc.nextInt();
+      System.out.println("Enter Student CGPA:");
+      float cgpa = sc.nextFloat();
+      pstmt.setInt(1,sno);
+      pstmt.setString(2,sname);
+      pstmt.setFloat(3,cgpa);
+      rows =pstmt.executeUpdate();
+    }
     if (rows>0){
       System.out.println(rows+"Rows Inserted");
     }
